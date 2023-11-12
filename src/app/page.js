939f39aad419dd5736e678c27e2945401c081b0d
ccalvinhuang <ccalvinhhuang@gmail.com>
2023-11-12 14:54:39 -0500

    Final prototype profile

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,13 @@
 import Image from 'next/image'
 import HeroSection from './components/HeroSection'
+import { Container } from 'postcss'
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col bg-black container mx-auto px-12 py-4">
-      <HeroSection />
+    <main className="flex min-h-screen flex-col bg-black">
+      <container class = "container mx-auto px-12 py-4">
+        <HeroSection />
+      </container>
     </main>
   )
 }
