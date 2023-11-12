3e5c0b289a904407566a2a1e57f9f34c8f49de4b
ccalvinhuang <ccalvinhhuang@gmail.com>
2023-11-12 14:29:36 -0500

    fixed columns

--- src/app/components/HeroSection.jsx
+++ src/app/components/HeroSection.jsx
@@ -11,18 +11,17 @@ const HeroSection = () => {
                         CS @ Georgia Tech | AI/ML Researcher
                     </p>
                 </div>
-            </div>
-            <div className="col-span-5 place-self-center mt-4 lg:mt-0">
-                <div className="bg-black w-[250px] h-[250px] relative">
-                    <Image
-                        src = "/images/hero-image.jpeg"
-                        alt = "hero image"
-                        className="absolute transform -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2"
-                        width = {300}
-                        height = {300}
-                    />
+                <div className="col-span-5 place-self-center mt-4 lg:mt-0">
+                    <div className="bg-black w-[250px] h-[250px] lg:w-[400px] lg:h-[400px] relative">
+                        <Image
+                            src = "/images/hero-image.jpeg"
+                            alt = "hero image"
+                            className="absolute transform -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2"
+                            width = {300}
+                            height = {300}
+                        />
+                    </div>
                 </div>
-                
             </div>
         </section>
     )
