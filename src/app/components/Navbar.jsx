fd61993e2be14ccfebeee992e5561dcc12fe47de
ccalvinhuang <ccalvinhhuang@gmail.com>
2023-11-14 00:50:44 -0500

    much cleaner

--- src/app/components/Navbar.jsx
+++ src/app/components/Navbar.jsx
@@ -2,14 +2,13 @@ import Link from "next/link";
 import React from "react";
 
 const Navbar = () => {
-    return (
-        <div className="flex flex-wrap items-center justify-between mx-auto p-8">
-            <Link href={"/"} className="text-2xl text-white font-semibold">
-                Calvin Huang 
-            </Link>
-            
-        </div>
-    );
+  return (
+    <div className="flex flex-wrap items-center justify-between  p-8">
+      <Link href={"/"} className="text-2xl text-white font-semibold">
+        Calvin Huang
+      </Link>
+    </div>
+  );
 };
 
 export default Navbar;
--- src/app/page.js
+++ src/app/page.js
@@ -1,14 +1,12 @@
-import Image from 'next/image'
-import HeroSection from './components/HeroSection'
-import { Container } from 'postcss'
-import Navbar from './components/Navbar'
+import Image from "next/image";
+import HeroSection from "./components/HeroSection";
+import { Container } from "postcss";
+import Navbar from "./components/Navbar";
 export default function Home() {
   return (
-    <main className=" bg-black">
+    <main className="flex flex-col bg-black">
       <Navbar />
-      <div class = "container mx-auto px-12 py-4">
-        <HeroSection />
-      </div>
+      <HeroSection />
     </main>
-  )
+  );
 }
