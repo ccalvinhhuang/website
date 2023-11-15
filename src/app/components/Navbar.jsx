0379aa90a7f595f928d0cd2f3ef645a65b5c7ae6
ccalvinhuang <ccalvinhhuang@gmail.com>
2023-11-15 02:04:11 -0500

    horizontal nav bar

--- src/app/components/Navbar.jsx
+++ src/app/components/Navbar.jsx
@@ -4,12 +4,12 @@ import React from "react";
 const Navbar = () => {
   return (
     <nav>
-      <div className="flex flex-wrap items-center justify-between  p-8">
+      <div className="flex flex-wrap items-center justify-between p-8">
         <Link href={"/"} className="text-2xl text-white font-semibold">
           Calvin Huang
         </Link>
         <div className="menu hidden md:block md:w-auto" id="navbar">
-          <ul>
+          <ul className="flex gap-10">
             <li>
               <Link
                 href={"#about"}
@@ -32,6 +32,22 @@ const Navbar = () => {
                 className="block py-2 pl-3 pr-4 text-[#ADB7BE] sm:text-l rounded md:p-0 hover:"
               >
                 Projects
+              </Link>
+            </li>
+            <li>
+              <Link
+                href={"#honors"}
+                className="block py-2 pl-3 pr-4 text-[#ADB7BE] sm:text-l rounded md:p-0 hover:"
+              >
+                Honors
+              </Link>
+            </li>
+            <li>
+              <Link
+                href={"#skills"}
+                className="block py-2 pl-3 pr-4 text-[#ADB7BE] sm:text-l rounded md:p-0 hover:"
+              >
+                Skills
               </Link>
             </li>
           </ul>
