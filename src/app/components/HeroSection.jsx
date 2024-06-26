7a5baea527f64c5ee97ed14a4f9a3f7c01c150b4
ccalvinhuang <ccalvinhhuang@gmail.com>
2024-06-25 21:29:18 -0700

    added expansion effect to each experience

--- src/app/components/HeroSection.jsx
+++ src/app/components/HeroSection.jsx
@@ -92,118 +92,178 @@ const HeroSection = () => {
         Experience
       </h2>
       <div className="mt-10">
-        <div className="text-white mb-10">
-          <div className="flex flex-col sm:flex-row items-center gap-8 sm:gap-48">
-            <div>
-              <h3 className="text-xl mb-2">Software Engineer Intern</h3>
-              <p className="text-[#ADB7BE] mb-4">OKSI | Apr 2024 - Current</p>
-              <p className="text-base mb-10 w-96">
-                Designed LLM-backed assistant for filling SCIF forms by
-                integrating RAG, LLaMA, AWQ, LoRA.
-              </p>
-            </div>
-            <div className="flex justify-center w-full -mt-10">
-              <Image
-                src="/images/oksi_logo.jpeg"
-                alt="eic image"
-                width={100}
-                height={100}
-                className="mx-auto"
-              />
+        <div className="mt-10">
+          <div className="text-white mb-10">
+            <div className="flex flex-col sm:flex-row items-center gap-8 sm:gap-48 hover-effect">
+              <div>
+                <h3 className="text-xl mb-2">Software Engineer Intern</h3>
+                <p className="text-[#ADB7BE] mb-4">OKSI | Apr 2024 - Current</p>
+                <p className="text-base mb-10 w-96">
+                  Designed LLM-backed assistant for filling SCIF forms by
+                  integrating RAG, LLaMA, AWQ, LoRA.
+                </p>
+              </div>
+              <div className="flex justify-center w-full -mt-10">
+                <Image
+                  src="/images/oksi_logo.jpeg"
+                  alt="oksi image"
+                  width={100}
+                  height={100}
+                  className="mx-auto"
+                />
+              </div>
             </div>
           </div>
+          <style jsx>{`
+            .hover-effect {
+              transition: transform 0.3s ease, box-shadow 0.3s ease;
+            }
+
+            .hover-effect:hover {
+              transform: scale(1.05);
+              box-shadow: 0 10px 20px rgba(0, 0, 0, 0.2);
+            }
+          `}</style>
         </div>
-        <div className="text-white mb-10">
-          <div className="flex flex-col sm:flex-row items-center gap-8 sm:gap-48">
-            <div>
-              <h3 className="text-xl mb-2">FTTP</h3>
-              <p className="text-[#ADB7BE] mb-4">
-                Jane Street Capital | Mar 2024 - Mar 2024
-              </p>
-              <p className="text-base mb-10 w-96">
-                Selected to explore Jane Street’s trading models through
-                classes, games, and simulations.
-              </p>
-            </div>
-            <div className="flex justify-center w-full -mt-10">
-              <Image
-                src="/images/jslogocolored.jpeg"
-                alt="eic image"
-                width={100}
-                height={100}
-                className="mx-auto"
-              />
+        <div className="mt-10">
+          <div className="text-white mb-10">
+            <div className="flex flex-col sm:flex-row items-center gap-8 sm:gap-48 hover-effect">
+              <div>
+                <h3 className="text-xl mb-2">FTTP</h3>
+                <p className="text-[#ADB7BE] mb-4">
+                  Jane Street Capital | Mar 2024 - Mar 2024
+                </p>
+                <p className="text-base mb-10 w-96">
+                  Selected to explore Jane Street’s trading models through
+                  classes, games, and simulations.
+                </p>
+              </div>
+              <div className="flex justify-center w-full -mt-10">
+                <Image
+                  src="/images/jslogocolored.jpeg"
+                  alt="oksi image"
+                  width={100}
+                  height={100}
+                  className="mx-auto"
+                />
+              </div>
             </div>
           </div>
+          <style jsx>{`
+            .hover-effect {
+              transition: transform 0.3s ease, box-shadow 0.3s ease;
+            }
+
+            .hover-effect:hover {
+              transform: scale(1.05);
+              box-shadow: 0 10px 20px rgba(0, 0, 0, 0.2);
+            }
+          `}</style>
         </div>
-        <div className="text-white mb-10">
-          <div className="flex flex-col sm:flex-row items-center gap-8 sm:gap-48">
-            <div>
-              <h3 className="text-xl mb-2">Machine Learning Intern</h3>
-              <p className="text-[#ADB7BE] mb-4">
-                EIC Lab @ Georgia Tech | Sep 2023 - Current
-              </p>
-              <p className="text-base mb-10 w-96">
-                Optimized custom GraphSAGE models using PyTorch to enhance GNN
-                node classification.
-              </p>
-            </div>
-            <div className="flex justify-center w-full -mt-10">
-              <Image
-                src="/images/eiclab.png"
-                alt="eic image"
-                width={100}
-                height={100}
-                className="mx-auto"
-              />
+        <div className="mt-10">
+          <div className="text-white mb-10">
+            <div className="flex flex-col sm:flex-row items-center gap-8 sm:gap-48 hover-effect">
+              <div>
+                <h3 className="text-xl mb-2">Machine Learning Intern</h3>
+                <p className="text-[#ADB7BE] mb-4">
+                  EIC Lab @ Georgia Tech | Sep 2023 - Current
+                </p>
+                <p className="text-base mb-10 w-96">
+                  Optimized custom GraphSAGE models using PyTorch to enhance GNN
+                  node classification.
+                </p>
+              </div>
+              <div className="flex justify-center w-full -mt-10">
+                <Image
+                  src="/images/eiclab.png"
+                  alt="oksi image"
+                  width={100}
+                  height={100}
+                  className="mx-auto"
+                />
+              </div>
             </div>
           </div>
+          <style jsx>{`
+            .hover-effect {
+              transition: transform 0.3s ease, box-shadow 0.3s ease;
+            }
+
+            .hover-effect:hover {
+              transform: scale(1.05);
+              box-shadow: 0 10px 20px rgba(0, 0, 0, 0.2);
+            }
+          `}</style>
         </div>
-        <div className="text-white mb-10">
-          <div className="flex flex-col sm:flex-row items-center gap-8 sm:gap-48">
-            <div>
-              <h3 className="text-xl mb-2">Data Science Intern</h3>
-              <p className="text-[#ADB7BE] mb-4">
-                Park Lab @ UC Irvine | Aug 2022 - Sep 2023
-              </p>
-              <p className="text-base mb-10 w-96">
-                Analyzed correlations between AMPA exposure, hormonal blood
-                levels and c-reactive proteins.
-              </p>
-            </div>
-            <div className="flex justify-center w-full -mt-10">
-              <Image
-                src="/images/ucischoolimage.jpeg"
-                alt="park image"
-                width={100}
-                height={100}
-                className="mx-auto"
-              />
+        <div className="mt-10">
+          <div className="text-white mb-10">
+            <div className="flex flex-col sm:flex-row items-center gap-8 sm:gap-48 hover-effect">
+              <div>
+                <h3 className="text-xl mb-2">Data Science Intern</h3>
+                <p className="text-[#ADB7BE] mb-4">
+                  Park Lab @ UC Irvine | Aug 2022 - Sep 2023
+                </p>
+                <p className="text-base mb-10 w-96">
+                  Analyzed correlations between AMPA exposure, hormonal blood
+                  levels and c-reactive proteins.
+                </p>
+              </div>
+              <div className="flex justify-center w-full -mt-10">
+                <Image
+                  src="/images/ucischoolimage.jpeg"
+                  alt="oksi image"
+                  width={100}
+                  height={100}
+                  className="mx-auto"
+                />
+              </div>
             </div>
           </div>
+          <style jsx>{`
+            .hover-effect {
+              transition: transform 0.3s ease, box-shadow 0.3s ease;
+            }
+
+            .hover-effect:hover {
+              transform: scale(1.05);
+              box-shadow: 0 10px 20px rgba(0, 0, 0, 0.2);
+            }
+          `}</style>
         </div>
-        <div className="text-white mb-10">
-          <div className="flex flex-col sm:flex-row items-center gap-8 sm:gap-48">
-            <div>
-              <h3 className="text-xl mb-2">Software Engineer Intern</h3>
-              <p className="text-[#ADB7BE] mb-4">
-                Softcom Lab | Apr 2023 - Jul 2023
-              </p>
-              <p className="text-base mb-10 w-96">
-                Designed a training pipeline for fine-tuning YOLOv5 on custom
-                data with QLoRA.
-              </p>
-            </div>
-            <div className="flex justify-center w-full -mt-10">
-              <Image
-                src="/images/softcomlab.png"
-                alt="softcomlab image"
-                width={100}
-                height={100}
-                className="mx-auto"
-              />
+        <div className="mt-10">
+          <div className="text-white mb-10">
+            <div className="flex flex-col sm:flex-row items-center gap-8 sm:gap-48 hover-effect">
+              <div>
+                <h3 className="text-xl mb-2">Software Engineer Intern</h3>
+                <p className="text-[#ADB7BE] mb-4">
+                  Softcom Lab | Apr 2023 - Jul 2023
+                </p>
+                <p className="text-base mb-10 w-96">
+                  Designed a training pipeline for fine-tuning YOLOv5 on custom
+                  data with QLoRA.
+                </p>
+              </div>
+              <div className="flex justify-center w-full -mt-10">
+                <Image
+                  src="/images/softcomlab.png"
+                  alt="oksi image"
+                  width={100}
+                  height={100}
+                  className="mx-auto"
+                />
+              </div>
             </div>
           </div>
+          <style jsx>{`
+            .hover-effect {
+              transition: transform 0.3s ease, box-shadow 0.3s ease;
+            }
+
+            .hover-effect:hover {
+              transform: scale(1.05);
+              box-shadow: 0 10px 20px rgba(0, 0, 0, 0.2);
+            }
+          `}</style>
         </div>
       </div>
     </div>
