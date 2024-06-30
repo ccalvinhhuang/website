abd9573b6d5193dd0dc60f862fd93bd1711c696a
ccalvinhuang <ccalvinhhuang@gmail.com>
2024-06-30 00:15:57 -0700

    completed general projects content

--- src/app/components/Projects.jsx
+++ src/app/components/Projects.jsx
@@ -64,39 +64,77 @@ const projects = [
     title: "Ruby",
     description:
       "A Google Chrome extension which helps Cantonese-speaking elders navigate the English web via queries to a fine-tuned Whisper and LLaMa model.",
-    tags: ["JavaScript", "React", "React Router", "Axios", "YouTube API"],
+    tags: [
+      "PyTorch",
+      "PostgreSQL",
+      "LLaMA",
+      "LoRA",
+      "Unsloth",
+      "Javascript",
+      "Docker",
+    ],
   },
   {
     title: "Ariel Chat",
     description:
       "Augmented LLaMA-3 with AWQ and RAG (dragon multiturn encoder/tokenizer) to develop a Turbo Tax-esque interface for users to fill out a DCSA 147.",
-    tags: ["Gatsby", "Sublime Text", "Atom", "VS Code", "iTerm2", "Hyper"],
+    tags: [
+      "Pytorch",
+      "PostgreSQL",
+      "Docker",
+      "AWQ",
+      "Huggingface",
+      "NATS",
+      "Protobuffs",
+      "React",
+    ],
   },
   {
     title: "ScribeAI",
     description:
       "[1st Place Overall @ HackGT X] A HIPAA-Compliant interface converting physical healthcare documents into Electronic Health Records with OCR and GPT-3.5.",
-    tags: ["Python", "MySQL", "Flask", "HTML", "CSS", "JavaScript"],
+    tags: [
+      "NextJS",
+      "PostgreSQL",
+      "Spring",
+      "Docker",
+      "Kubernetes",
+      "NodeJS",
+      "Tensorflow",
+    ],
   },
   {
     title: "NE-GraphSAGE: Memory Efficient GNN Training with Neighbor Ensemble",
     description:
       "Integrating neighbor ensemble into GraphSAGE with novel dropout strategies and MLP distributions to achieve higher accuracy.",
-    tags: ["Python", "MySQL", "Flask", "HTML", "CSS", "JavaScript"],
+    tags: ["PyTorch", "Optuna", "Docker", "DGL", "CUDA"],
+  },
+  {
+    title: "Personal Website",
+    description:
+      "The absolutely sexy website you are currently viewing. Built with crazy amounts of front-end dev from scratch, deployed on Vercel.",
+    tags: [
+      "NextJS",
+      "TailwindCSS",
+      "GraphQL",
+      "NodeJS",
+      "Vercel",
+      "PostgreSQL",
+    ],
   },
   {
     title:
       "Association of Glyphosate and AMPA  with Kidney Function and Hormonal Markers.",
     description:
       "Assessed relationships between  glyphosate and AMPA concentrations and biomarkers such as sex hormones and hsCRP via Spearman's, ANOVA, and Chi-Squared.",
-    tags: ["Python", "MySQL", "Flask", "HTML", "CSS", "JavaScript"],
+    tags: ["R", "Javascript", "Python"],
   },
   {
     title:
       "Generative Approach to the Automation of Artificial Intelligence Applications",
     description:
-      "Developed a training pipeline for a ResNet model using TensorFlow Lite, enabling training and inference on mobile devices through cloud integration.",
-    tags: ["Python", "MySQL", "Flask", "HTML", "CSS", "JavaScript"],
+      "Developed a training pipeline for ResNet50 using TensorFlow Lite, enabling training and inference on mobile devices through cloud integration.",
+    tags: ["Tensorflow Lite", "AWS EC2", "Flask", "MySQL", "REST"],
   },
 ];
 
